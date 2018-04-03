083e752e3e62ebfca33d13c036c8fdf6aac0195d
Jay Yang <hjyang1@uci.edu>
2018-04-03 14:14:54 -0700

    maze running setting set up

---
+++ MazeRunner/Assets/Brain.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Brain : MonoBehaviour {
+
+    int DNALength = 2;
+
+    public GameObject eyes;
+    
+    public DNA dna;
+    bool seeWall = true;
+    bool alive = true;
+    public float disFromStart;
+    public Vector3 startPos;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "dead")
+        {
+            alive = false;
+        }
+    }
+
+    public void Init()
+    {
+        //0 forward
+        //1 left
+        //2 right
+
+        dna = new DNA(DNALength, 3);
+        disFromStart = 0;
+        alive = true;
+    }
+
+
+	// Update is called once per frame
+	void Update () {
+		if (!alive)
+        {
+            return;
+        }
+
+        Debug.DrawRay(eyes.transform.position, eyes.transform.forward * 1, Color.red, 10);
+        seeWall = false;
+        RaycastHit hit;
+        if (Physics.Raycast(eyes.transform.position, eyes.transform.forward, out hit))
+        {
+            if (hit.collider.gameObject.tag == "wall")
+            {
+                seeWall = true;
+            }
+        }
+
+        float turn = 0;
+        float move = 0;
+
+        if (seeWall)
+        {
+            if (dna.GetGene(1) == 0) move = 1;
+            else if (dna.GetGene(1) == 1) turn = -90;
+            else if (dna.GetGene(1) == 2) turn = 90;
+        }
+
+        else
+        {
+            if (dna.GetGene(0) == 0) move = 1;
+            else if (dna.GetGene(0) == 1) turn = -90;
+            else if (dna.GetGene(0) == 2) turn = 90;
+        }
+
+        this.transform.Translate(0, 0, move * .1f);
+        this.transform.Rotate(0, turn, 0);
+
+        disFromStart = Vector3.Distance(startPos, this.transform.position);
+    }
+}
---
+++ MazeRunner/Assets/PopulationManager.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class PopulationManager : MonoBehaviour {
+
+    public GameObject botPrefab;
+    public int populationSize = 50;
+    List<GameObject> population = new List<GameObject>();
+    public static float elapsed = 0;
+    public float trialTime = 20;
+    int generation = 1;
+
+    GUIStyle guiStyle = new GUIStyle();
+    private void OnGUI()
+    {
+        guiStyle.fontSize = 25;
+        guiStyle.normal.textColor = Color.white;
+        GUI.BeginGroup(new Rect(10, 10, 250, 150));
+        GUI.Box(new Rect(0, 0, 140, 140), "Stats", guiStyle);
+        GUI.Label(new Rect(10, 25, 200, 30), "Gen: " + generation, guiStyle);
+        GUI.Label(new Rect(10, 50, 200, 30), string.Format("Time: {0:0.00}", elapsed), guiStyle);
+        GUI.Label(new Rect(10, 75, 200, 30), "Population: " + population.Count, guiStyle);
+        GUI.EndGroup();
+
+    }
+    // Use this for initialization
+    void Start() {
+        for (int i = 0; i < populationSize; i++)
+        {
+            Vector3 startingPos = new Vector3(this.transform.position.x + Random.Range(-2, 2),
+                this.transform.position.y,
+                this.transform.position.z + Random.Range(-2, 2));
+
+            GameObject b = Instantiate(botPrefab, startingPos, this.transform.rotation);
+            b.GetComponent<Brain>().Init();
+            population.Add(b);
+        }
+		
+	}
+
+    GameObject Breed(GameObject parent1, GameObject parent2)
+    {
+        Vector3 startingPos = new Vector3(this.transform.position.x + Random.Range(-2, 2),
+                this.transform.position.y,
+                this.transform.position.z + Random.Range(-2, 2));
+
+        GameObject offSpring = Instantiate(botPrefab, startingPos, this.transform.rotation);
+        offSpring.GetComponent<Brain>().Init();
+
+        return offSpring;
+    }
+
+    void BreedNewPopulation()
+    {
+        List<GameObject> sortedList = population.OrderBy(o => o.GetComponent<Brain>().disFromStart).ToList();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
